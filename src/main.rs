ec854736dd1bbcc2f674fa3cdb501aefc9aeecfa
hpodl <hubert.podlewski.stud@pw.edu.pl>
2023-02-22 19:34:26 +0100

    Example of 2 communicating clients

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,55 @@
+use std::cell::RefCell;
+use std::net::SocketAddr;
+use std::ops::Deref;
+use std::str::FromStr;
+use std::sync::Arc;
 use std::time::Duration;
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufStream};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
 use tokio::time;
 
 const PACKET_SIZE: usize = 2;
 
 pub struct Client {
-    data: Vec<u8>,
+    data: Mutex<RefCell<Vec<u8>>>,
+    address: SocketAddr,
 }
 
 impl Client {
-    pub fn new() -> Self {
-        Self { data: Vec::new() }
-    }
-
-    pub fn data(&self) -> &[u8] {
-        self.data.as_ref()
-    }
-
-    pub fn set_data(&mut self, data: Vec<u8>) {
-        self.data = data;
-    }
-
-    async fn send(&self) -> std::io::Result<()> {
-        let mut stream = TcpStream::connect("localhost:4877").await?;
-
-        for &byte in &self.data {
-            println!("Wrote {} bytes", stream.write(&[byte]).await?);
+    pub fn new(address: SocketAddr) -> Self {
+        Self {
+            data: Mutex::new(RefCell::new(Vec::new())),
+            address,
         }
+    }
+
+    pub async fn get_data(&self, from: usize, count: usize) -> Vec<u8> {
+        println!("GetData waiting for lock");
+        let lock = self.data.lock().await;
+        println!("GetData got the lock");
+
+        let data_copied = lock.deref().borrow()[from..from + count].to_vec();
+        data_copied
+    }
+
+    pub async fn set_data(&mut self, data: Vec<u8>) {
+        let lock = self.data.get_mut();
+        *lock.deref().borrow_mut() = data;
+    }
+
+    pub async fn send(&self, to: SocketAddr) -> std::io::Result<()> {
+        let mut stream = TcpStream::connect(to).await?;
+
+        let packet = self.get_data(0, PACKET_SIZE).await;
+        println!("Wrote {} bytes", stream.write(&packet).await?);
+
         Ok(())
     }
 
-    async fn listen(&mut self) -> std::io::Result<()> {
-        let listener = TcpListener::bind("localhost:4877").await?;
+    async fn listen(&self) -> std::io::Result<()> {
+        let listener = TcpListener::bind(self.address).await?;
 
         let mut packet_buffer = [0u8; PACKET_SIZE];
 
@@ -41,7 +57,12 @@ impl Client {
             let mut buffered_stream = BufStream::new(stream);
 
             while let Ok(bytes_read) = buffered_stream.read_exact(&mut packet_buffer).await {
-                self.data.extend_from_slice(&packet_buffer[..bytes_read]);
+                println!("Listener waiting for lock");
+                let lock = self.data.lock().await;
+                println!("Listener got the lock");
+                lock.deref()
+                    .borrow_mut()
+                    .extend_from_slice(&packet_buffer[..bytes_read]);
             }
 
             println!("Data now: {:?}", self.data);
@@ -52,20 +73,50 @@ impl Client {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let mut sender = Client::new();
-    sender.set_data("Message.".as_bytes().to_owned());
+    let first_addr = SocketAddr::from_str("127.0.0.50:2137").unwrap();
+    let second_addr = SocketAddr::from_str("127.0.0.51:2137").unwrap();
 
-    let listener_handle = tokio::spawn(async {
-        let mut listener = Client::new();
-        listener.listen().await
+    let mut first_client = Client::new(first_addr);
+    let mut second_client = Client::new(second_addr);
+
+    first_client.set_data("Msg.".as_bytes().to_owned()).await;
+    second_client.set_data("eae.".as_bytes().to_owned()).await;
+
+    // First client setup
+    let client_ptr = Arc::new(first_client);
+
+    let _listener_handle_1 = tokio::spawn({
+        let ptr_cloned = Arc::clone(&client_ptr);
+        async move { ptr_cloned.listen().await }
     });
 
-    time::sleep(Duration::from_millis(100)).await;
+    let sender_handle_1 = tokio::spawn({
+        let ptr_cloned = Arc::clone(&client_ptr);
+        async move {
+            tokio::time::sleep(Duration::from_millis(250)).await;
+            ptr_cloned.send(second_addr).await
+        }
+    });
 
-    sender.send().await?;
-    sender.send().await?;
+    // Second client setup
+    let client_ptr = Arc::new(second_client);
 
-    listener_handle.await??;
+    let _listener_handle_2 = tokio::spawn({
+        let ptr_cloned = Arc::clone(&client_ptr);
+        async move { ptr_cloned.listen().await }
+    });
+
+    let sender_handle_2 = tokio::spawn({
+        let ptr_cloned = Arc::clone(&client_ptr);
+        async move {
+            tokio::time::sleep(Duration::from_millis(250)).await;
+            ptr_cloned.send(first_addr).await
+        }
+    });
+
+    sender_handle_1.await??;
+    sender_handle_2.await??;
+
     Ok(())
 }
 

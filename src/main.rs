b42348a002a79d4ec938004e1a9143219048fc77
hpodl <hubert.podlewski.stud@pw.edu.pl>
2023-02-22 18:12:13 +0100

    Writable data

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use std::net::SocketAddr;
 use std::time::Duration;
 
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufStream};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::time::{self};
 
-pub struct Client {}
+pub struct Client {
+    data: Vec<u8>,
+}
 
 impl Client {
     pub fn new() -> Self {
-        Self {}
+        Self { data: Vec::new() }
+    }
+
+
+    pub fn data(&self) -> &[u8] {
+        self.data.as_ref()
+    }
+
+    pub fn set_data(&mut self, data: Vec<u8>) {
+        self.data = data;
     }
 
     async fn send(&mut self) -> std::io::Result<()> {
-        println!("Sending");
         let mut stream = TcpStream::connect("localhost:4877").await?;
-        stream.write("Abcdef\n".as_bytes()).await?;
+
+        for &byte in &self.data {
+            println!("Wrote {} bytes", stream.write(&[byte]).await?);
+        }
         Ok(())
     }
 
@@ -24,11 +36,11 @@ impl Client {
 
         while let Ok((stream, _)) = listener.accept().await {
             let buffered_stream = BufStream::new(stream);
-
+            
             let mut lines = buffered_stream.lines();
             while let Ok(maybe_line) = lines.next_line().await {
                 match maybe_line {
-                    Some(line) => println!("{}", line),
+                    Some(line) => println!("Read {}.", line),
                     None => break,
                 }
             }
@@ -40,17 +52,20 @@ impl Client {
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let mut sender = Client::new();
+    sender.set_data("Message.".as_bytes().to_owned());
 
     let listener_handle = tokio::spawn(async {
         let mut listener = Client::new();
         listener.listen().await
     });
+
     time::sleep(Duration::from_millis(100)).await;
 
-    for _ in 1..10 {
-        sender.send().await?;
-    }
+    sender.send().await?;
 
     listener_handle.await??;
     Ok(())
 }
+
+#[cfg(test)]
+mod test {}

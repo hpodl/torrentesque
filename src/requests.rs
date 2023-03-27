7c2e9dfc55f288ad0c4908db546598eac4d4cba9
hpodl <hubert.podlewski.stud@pw.edu.pl>
2023-03-27 06:35:17 +0200

    Commented request/response enums

--- src/requests.rs
+++ src/requests.rs
@@ -3,12 +3,14 @@ use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
 #[derive(Serialize, Deserialize)]
+/// Requests peers send to a tracker
 pub enum RequestToTracker {
     GetPeers,
     RegisterAsPeer(SocketAddr),
 }
 
 #[derive(Serialize, Deserialize)]
+/// Tracker's response to a peer's request (`RequestToTracker`)
 pub enum TrackerResponse {
     Peers(Vec<SocketAddr>),
     InvalidRequest,
@@ -16,12 +18,14 @@ pub enum TrackerResponse {
 }
 
 #[derive(Serialize, Deserialize)]
+/// Requests a leech makes to its peers
 pub enum LeechRequest {
     GetAvailability,
     GetPackets(usize, usize),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+/// Seed's response to leech's request (`LeechRequest`)
 pub enum SeedResponse {
     InvalidRequest,
     Availability(BitVec),
